961602ad231cbf58365c7195a06142d00dafcff4
jmsbaduor <jmsbaduor@gmail.com>
2019-01-27 10:07:43 +0000

    This is the first commit

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var UITextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "This is the New Text"
+        UITextField.becomeFirstResponder()
+        
     }
-
-
+    
+    
+    @IBAction func buttonWasTapped(_ sender: Any) {
+        
+        label.text = "Hello \(UITextField.text!)"
+        UITextField.resignFirstResponder()
+        
+    }
+    
 }
 
